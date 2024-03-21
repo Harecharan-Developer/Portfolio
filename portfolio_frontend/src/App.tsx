acdf6cddb9f119ff78ded39f7666d4a74eb006b2
Harecharan-Developer <harecharan321@gmail.com>
2024-03-21 18:39:47 +0530

    enter passscode and make a button appear

--- portfolio_frontend/src/App.tsx
+++ portfolio_frontend/src/App.tsx
@@ -2,9 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ButtonWebsite from './buttonwebsite'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const EntryFlag = count === 1 && count1 === 0 && count2 === 1;
 
   return (
     <>
@@ -16,20 +20,31 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Enter the code to get started</h1>
+      <h4>Clue: to know the code, do what you do when something is not rendering (as a non-programmer)</h4>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(prevCount => prevCount + 1)}>
           count is {count}
+        </button>
+        <button onClick={() => setCount1(prevCount => prevCount + 1)}>
+          count is {count1}
+        </button>
+        <button onClick={() => setCount2(prevCount => prevCount + 1)}>
+          count is {count2}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <div>
+        {EntryFlag && <ButtonWebsite />}
+      </div>
+      
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ portfolio_frontend/src/buttonwebsite.tsx
@@ -0,0 +1,11 @@
+function ButtonWebsite() {
+    return (
+        <div>
+            <a href="./index2.html" target="_blank" rel="noopener noreferrer">
+                <button>Click here to go to the website</button>
+            </a>
+        </div>
+    );
+}
+
+export default ButtonWebsite;
